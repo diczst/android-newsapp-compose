4195cca186a68fa9a6d6bf286d3c3f86009536f8
dickyzuerst <dzuerst@gmail.com>
2024-10-12 20:54:12 +0700

    refactoring

--- app/src/main/java/com/neonusa/newsapp/domain/usecases/app_entry/ReadAppEntry.kt
+++ app/src/main/java/com/neonusa/newsapp/domain/usecases/app_entry/ReadAppEntry.kt
@@ -6,7 +6,6 @@ import kotlinx.coroutines.flow.Flow
 class ReadAppEntry(
     private val localUserManger: LocalUserManager
 ) {
-
     operator fun invoke(): Flow<Boolean> {
         return localUserManger.readAppEntry()
     }
